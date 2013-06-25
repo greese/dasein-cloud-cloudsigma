060db93943733842f4e08682c40e648888161bd7
Danielle Mayne <danielle.mayne@enstratus.com>
2013-06-25 15:35:35 +0100

    switch round stop commands (stop is a hard 'power off'/force)

--- src/main/java/org/dasein/cloud/cloudsigma/compute/vm/ServerSupport.java
+++ src/main/java/org/dasein/cloud/cloudsigma/compute/vm/ServerSupport.java
@@ -1180,9 +1180,9 @@ public class ServerSupport implements VirtualMachineSupport {
             }
 
             if (force) {
-                method.postString(toServerURL(vmId, "action/?do=shutdown"), "");
-            } else {
                 method.postString(toServerURL(vmId, "action/?do=stop"), "");
+            } else {
+                method.postString(toServerURL(vmId, "action/?do=shutdown"), "");
             }
             //dmayne 20130528: wait for server to be stopped
             // as some activities require this state before continuing
